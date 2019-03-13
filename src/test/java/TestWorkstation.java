b5d781ba08157dc2c5cfbdc20c7a0a5f5627432d
Lun Tan <tanlun1991@gmail.com>
2019-03-13 01:22:29 -0400

    starting java for mac automation

---
+++ src/test/java/TestWorkstation.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestWorkstation {
+    @Test
+    public void testWorkstationSearch() throws InterruptedException, MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platform","Mac");
+
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4622/wd/hub"),capabilities);
+        driver.get("MicroStrategy Workstation");
+
+//        String documentAXpath = "/AXApplication[@AXTitle='MicroStrategy Workstation']";
+//        driver.findElement(By.xpath(documentAXpath));
+
+        Thread.sleep(2000);
+
+        String searchBoxAXpath = "/AXApplication[@AXTitle='MicroStrategy Workstation']/AXWindow[@AXTitle='MicroStrategy Workstation - 环境' and @AXSubrole='AXStandardWindow']/AXToolbar[0]/AXGroup[4]/AXTextField[@AXSubrole='AXSearchField']";
+        driver.findElement(By.xpath(searchBoxAXpath)).sendKeys("Airline");
+
+        Thread.sleep(2000);
+
+
+
+    }
+}
